20dbfe7928d000f07d916b3958d867140817701a
dilrun2001 <dilrun2001@gmail.com>
2025-09-15 02:20:48 +0900

    사하아이 요청준비 끝

--- Streamlit/util/llama3_korea_bllossomQ8.py
+++ Streamlit/util/llama3_korea_bllossomQ8.py
@@ -1,7 +1,81 @@
-# (이미 설치하셨으니 생략 가능)
+
+from ollama import Client, ResponseError
+from util.config import OLLAMA_HOST
 import util.state_copy as state_util
 import streamlit   as st
-# HF 허브에서 GGUF 파일만 내려받기
+
+def SahaAi_request(minwon,answer,answer_format,model_name: str = "gpt-oss:20b"):
+    system_prompt = """당신은 공공기관의 민원 응답을 담당하는 전문 공무원입니다.
+기관 이름은 '사하구청'입니다.
+
+다음의 **[답변 템플릿] 형식을 정확히 지켜서** 작성하십시오. 
+⚠️ [답변 템플릿]의 형식을 **그대로 유지하고**, 출력 시작 부분이나 끝에 어떠한 추가 설명도 하지 마십시오.
+
+※ 반드시 출력은 [답변 템플릿] 안에 있는 형식을 따르며, 템플릿 외 문장은 추가하지 마십시오.
+※ 템플릿 내 `[민원요지]` 부분은 민원 내용을 행정 문서 문체로 정중하게 요약하여 대체하되, `[민원요지]`라는 단어는 최종 출력에 **표시되지 않아야 합니다**.
+※ 템플릿 내 `[답변요지]` 부분은 답변 요지를 정중하고 행정적인 문체로 바꾸어 넣되, `[답변요지]`라는 단어는 최종 출력에 **표시되지 않아야 합니다**.
+※ 출력에는 들여쓰기, 불필요한 줄바꿈, 공백 없이 문장 맨 앞에서 바로 시작하십시오.
+"""
+    user_prompt = """
+다음은 민원에 대한 [답변 요지]와 [민원 내용]입니다.
+
+[민원 내용]
+""" + minwon + """
+
+[답변 요지]
+""" + answer + """
+
+[답변 템플릿]
+""" + answer_format
+    try:
+        client = Client(host=OLLAMA_HOST)
+        response = client.chat(
+            model= model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            stream=False,
+        )
+        content = response.get("message", {}).get("content", "")
+        print(content)
+        return content
+    except ResponseError as e:
+        print(f"❌ API 오류: {e.error}")
+    except Exception as e:
+        print(f"❌ 오류: {e}")
+
+def SahaAi_request_sub(minwon,model_name: str = "gpt-oss:20b"):
+    system_prompt = """당신은 민원 내용을 간결하게 요약하는 요약봇입니다.
+
+        [목표]
+        아래 [민원내용]을 읽고, 핵심 내용을 공무원이 빠르게 파악할 수 있도록 **한 줄 또는 두 줄로 요약**해 주세요.
+
+        [출력 형식]
+        - 요약문만 출력해 주세요.
+        - 접두사 없이, 바로 요약 내용만 작성하세요.
+        """
+    user_prompt = f"""[민원내용]{minwon}
+        """
+    try:
+        client = Client(host=OLLAMA_HOST)
+        response = client.chat(
+            model= model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            stream=False,
+        )
+        content = response.get("message", {}).get("content", "")
+        print(content)
+        return content
+    except ResponseError as e:
+        print(f"❌ API 오류: {e.error}")
+    except Exception as e:
+        print(f"❌ 오류: {e}")
+
+
 def AI_print_answer(minwon,answer,answer_format):
     from huggingface_hub import hf_hub_download
 
@@ -17,6 +91,7 @@ def AI_print_answer(minwon,answer,answer_format):
         )
     elif(st.session_state.model == '민원팩토리 모델'):
         model_path = "./util/llm_models/llama3_minwon_factory_q8_0/Llama3_Minwon_Factory-8.0B-Q8_0.gguf"
+
     
 
     # llama-cpp-python 으로 모델 로드 및 생성 함수 정의
--- Streamlit/util/menu.py
+++ Streamlit/util/menu.py
@@ -7,7 +7,7 @@ from util.state_copy import *
 def set_menu():
     with st.container(key = "llm_model_select", horizontal=True):
         popover =  st.popover("메뉴", icon= ":material/menu:")
-        model = popover.pills(":material/person: AI 모델 선택", options = ['기본 모델', '민원팩토리 모델', '사하아이 연동'], width = 450, default = '기본 모델')
+        model = popover.pills(":material/person: AI 모델 선택", options = ['기본 모델', '민원팩토리 모델', '사하아이 연동'], width = 450, default = '사하아이 연동')
         match (model):#, key = "llm_model_select", width = 300)):
             case '기본 모델':
                 if st.session_state.model != '기본 모델':
@@ -18,7 +18,10 @@ def set_menu():
                     st.toast(f"AI 모델이 변경되었습니다. {st.session_state.model} -> :green[민원팩토리 모델]", icon = ":material/check:")
                     st.session_state.model = '민원팩토리 모델'
             case '사하아이 연동':
-                st.toast( '''선택하신 설정은 현재 :red[지원하지 않는 설정]입니다.''', icon = ":material/block:")
+                #st.toast( '''선택하신 설정은 현재 :red[지원하지 않는 설정]입니다.''', icon = ":material/block:")
+                if st.session_state.model != '사하아이 연동':
+                    st.toast(f"AI 모델이 변경되었습니다. {st.session_state.model} -> :green[사하아이 연동]", icon = ":material/check:")
+                    st.session_state.model = '사하아이 연동'
         #popover.write('''---''')
         if st.session_state['page'] == "main":
             option_map = {
